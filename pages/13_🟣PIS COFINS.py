c962cdf6a00060dd0242840541af232a256e6225
renatacastellari-gif <renatacastellari@gmail.com>
2025-10-27 09:51:44 -0300

    Update 13_🟣PIS COFINS.py

--- pages/13_🟣PIS COFINS.py
+++ pages/13_🟣PIS COFINS.py
@@ -63,7 +63,7 @@ st.write("")
 st.write("")
 
 
-----------------------------
+st.markdown("---")
 
 # Texto explicativo em Markdown
 st.markdown("""
@@ -215,6 +215,7 @@ df = pd.DataFrame(data)
 st.subheader("📊 Devoluções com valor fiscal sem razão")
 st.dataframe(df.style.format(precision=2))
 	
+
 
 
 
