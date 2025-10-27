209f69c54c818376981456badd8949f767d26115
renatacastellari-gif <renatacastellari@gmail.com>
2025-10-27 09:01:17 -0300

    Update 13_🟣PIS COFINS.py

--- pages/13_🟣PIS COFINS.py
+++ pages/13_🟣PIS COFINS.py
@@ -21,7 +21,8 @@ Esta página apresenta as demonstrações das conciliações entre o ICMS e o Ra
 - Fonte Fiscal: Apuração Fiscal
 - Fonte Contábil: Conta 23000394 do razão extraída do SAP
 """)
-write("")
+st.write("")
+st.write("")
 
 st.markdown("""
  ⚠️ Diferença no Item **BASE LIQ FACIAL UP FPS 15 SOFT HONEY**
@@ -57,6 +58,7 @@ def highlight_dif(val):
 # Exibir no Streamlit
 st.subheader("📊 Diferença de Alíquota ICMS (Razão vs Apuração)")
 st.dataframe(df.style.format(precision=2).applymap(highlight_dif, subset=['Dif_Alíquota']))
+
 
 
 
