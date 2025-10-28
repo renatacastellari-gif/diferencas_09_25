702329d97c8549939704ab5b7280ec280dbdbc50
renatacastellari-gif <renatacastellari@gmail.com>
2025-10-28 11:15:38 -0300

    Update 15_🟣Resumo PIS COFINS.py

--- pages/15_🟣Resumo PIS COFINS.py
+++ pages/15_🟣Resumo PIS COFINS.py
@@ -53,17 +53,29 @@ st.image("Screenshot_3.png", width=1600)
 
 st.markdown("---")
 
-st.markdown("<p style='font-size:28px; font-weight:bold; color:#FFA500;'>PIS</p>", unsafe_allow_html=True)
+st.markdown(
+    "<p style='font-size:23px; font-weight:bold; color:#9B4DCC;'> 📝 RESUMO</p>",
+    unsafe_allow_html=True
+)
 
+# Texto explicativo
+st.markdown("""
+A base de cálculo utilizada nas notas fiscais — que também é refletida no razão contábil — considera o valor dos produtos somado ao frete, sem dedução do ICMS destacado.
+Já na apuração fiscal, aplica-se o abatimento do ICMS, reduzindo a base de cálculo para PIS e COFINS.
+""")
 
 
 
 # Seção 1: Comparativo Apuração vs Razão
-st.markdown("### ✅ Comparativo Apuração vs Razão")
+st.markdown(
+    "<p style='font-size:22px; font-weight:bold; color:#9B4DCC;'> ☑️ Comparativo Apuração vs Razão</p>",
+    unsafe_allow_html=True
+)
+
 
 dados_comparativo = {
     "Descrição": [
-        "Valor COFINS com abatimento do ICMS na base de cálculo (Apuração)",
+        "Valor PIS com abatimento do ICMS na base de cálculo (Apuração)",
         "(+) ICMS Próprio sobre as vendas (1,65% e 7,60%)",
         "(+) ICMS Próprio vendas Aliquota diferenciada (2,20% e 10,30%) Monofásico",
         "Valor Razão"
@@ -77,3 +89,4 @@ st.dataframe(df_comparativo.style.format({"Valor (R$)": "{:,.2f}"}))
 st.markdown("---")
 
 st.image("Screenshot_4.png", width=1600)
+
