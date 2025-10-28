ca3f99e4e38bd45abd1efa35dc2790836194902d
renatacastellari-gif <renatacastellari@gmail.com>
2025-10-28 15:01:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ else:
         ("PIS a Recuperar", "1280343"),
         ("COFINS a Recuperar", "1280344"),
         ("ICMS a Recuperar", "1280345"),
-        ("IPI a Recuperar", "1280345"),
+        ("IPI a Recuperar", "1280342"),
         ("VENDAS", "4000000"),
     ]
 
@@ -71,6 +71,7 @@ else:
 
     > **Objetivo:** Garantir o alinhamento entre os saldos fiscais e contábeis, prevenindo divergências nos registros.
     """)
+
 
 
 
