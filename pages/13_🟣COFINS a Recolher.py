53d031e2f1ef9964759ecabeaf99ebd08735f91b
renatacastellari-gif <renatacastellari@gmail.com>
2025-10-28 09:33:51 -0300

    Update 13_🟣COFINS a Recolher.py

--- pages/13_🟣COFINS a Recolher.py
+++ pages/13_🟣COFINS a Recolher.py
@@ -5,7 +5,7 @@ import pandas as pd
 import io
 
 
-st.set_page_config(layout="wide")
+
 # Configuração da página
 st.set_page_config(page_title="COFINS", page_icon="🟣")
 
@@ -249,6 +249,7 @@ st.dataframe(df_comparativo.style.format({"Valor (R$)": "{:,.2f}"}))
 st.markdown("---")
 
 st.image("Screenshot_3.png", width=1600)
+
 
 
 
