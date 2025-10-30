293c3ab7b499d0f61b12b6b3290ed34626f67a3a
renatacastellari-gif <renatacastellari@gmail.com>
2025-10-30 16:52:31 -0300

    Update 3_🟣 IPI.py

--- pages/3_🟣 IPI.py
+++ pages/3_🟣 IPI.py
@@ -18,6 +18,11 @@ st.markdown("""
     margin-bottom:20px;">
 IPI
 </h2>
+
+IPI
+</h2>
+""", unsafe_allow_html=True)
+
 st.markdown("**`IPI a RECOLHER - 2300390`**")
 
 st.markdown("""
@@ -136,6 +141,7 @@ st.markdown(custom_css + html_table, unsafe_allow_html=True)
 total_ipi = df["VLR_IPI"].sum(skipna=True)
 
 st.markdown(f"<p style='font-size:13px;'>💰 Valor total IPI: <b>R$ {total_ipi:.2f}</b></p>", unsafe_allow_html=True)
+
 
 
 
