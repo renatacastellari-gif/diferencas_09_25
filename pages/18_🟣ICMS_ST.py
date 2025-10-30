bca776a6850d21b0f597eafd6bd1a3693daa22d3
renatacastellari-gif <renatacastellari@gmail.com>
2025-10-30 17:01:09 -0300

    Update 18_🟣ICMS_ST.py

--- pages/18_🟣ICMS_ST.py
+++ pages/18_🟣ICMS_ST.py
@@ -2,13 +2,21 @@ import streamlit as st
 st.image('teste.svg', width=200) 
 
 
+# Título principal
 st.markdown("""
-## ICMS-ST 
-
- 
-             
-""")
+<h2 style="
+    color:#EEE4EF;
+    font-family:'Montserrat',sans-serif;
+    font-weight:700;
+    text-align:center;
+    border-bottom:2px solid #FFA500;
+    padding-bottom:8px;
+    margin-bottom:20px;">
+ICMS ST
+</h2>
+""", unsafe_allow_html=True)
 
 ("""**`Esta conta esta sendo conciliada pela colaboradora Gabriela. `** """)
+
 
 
