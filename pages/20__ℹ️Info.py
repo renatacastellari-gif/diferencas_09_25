34a42e1838b4cb13a624202c209ad97b01ab2cb9
renatacastellari-gif <renatacastellari@gmail.com>
2025-10-31 10:00:45 -0300

    Update 20__ℹ️Info.py

--- pages/20__ℹ️Info.py
+++ pages/20__ℹ️Info.py
@@ -76,8 +76,8 @@ st.markdown("""
 # ====== BOX DE INFORMAÇÃO ======
 st.markdown("""
 <div class='info-box'>
-As capas e detalhes completos estão salvos na rede, incluindo meses anteriores.<br>
-<span style="color:#FFA500; font-weight:600;">
+<span style="color:#FFA500; font-weight:600;"> 
+As capas e detalhes completos estão salvos na rede, incluindo meses anteriores.
 O objetivo desta página é proporcionar acesso rápido e facilitar a visualização das informações.
 </span>
 </div>
@@ -92,6 +92,7 @@ st.markdown("""
 Y:\\DEPTO CONTÁBIL\\BR02\\CONCILIAÇÕES\\Conciliações Renata
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
