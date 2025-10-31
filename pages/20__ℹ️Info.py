ccb493cff621a36b7de1dfd9b773c998af342215
renatacastellari-gif <renatacastellari@gmail.com>
2025-10-31 09:39:38 -0300

    Update 20__ℹ️Info.py

--- pages/20__ℹ️Info.py
+++ pages/20__ℹ️Info.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-st.image('teste.svg', width=300) 
-
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image("teste.svg", width=300)
 
 ("""
 
@@ -14,3 +15,4 @@ Esta página tem por objetivo **facilitar a visualização das conciliações do
 `Y:\DEPTO CONTÁBIL\BR02\CONCILIAÇÕES\Conciliações Renata`
 
 """)
+
