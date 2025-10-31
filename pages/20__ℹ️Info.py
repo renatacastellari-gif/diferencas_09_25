209bf23c381cc7c77f209bf044148012c865c2dc
renatacastellari-gif <renatacastellari@gmail.com>
2025-10-31 09:50:25 -0300

    Update 20__ℹ️Info.py

--- pages/20__ℹ️Info.py
+++ pages/20__ℹ️Info.py
@@ -1,10 +1,9 @@
 import streamlit as st
 
+# Centraliza a imagem
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
     st.image("teste.svg", width=300)
-
-
 
 # Título principal
 st.markdown("""
@@ -16,30 +15,18 @@ st.markdown("""
     border-bottom:2px solid #FFA500;
     padding-bottom:8px;
     margin-bottom:20px;">
-Informações Adicionais
-</h2> """)
-
-
+    Informações Adicionais
+</h2>
+""", unsafe_allow_html=True)
 
 # Segunda info
 st.info("""
 As capas e detalhes completos estão salvos na rede, incluindo meses anteriores.  
-  O objetivo do desenvolvimento dessa página é proporcionar acesso rápido e facilitar a visualização das informações.""")
-
-("""
- 
-
-📂 Caminho para acesso:  
-
-`Y:\DEPTO CONTÁBIL\BR02\CONCILIAÇÕES\Conciliações Renata`
-
+O objetivo do desenvolvimento dessa página é proporcionar acesso rápido e facilitar a visualização das informações.
 """)
 
-
-
-
-
-
-
-
-
+# Caminho de rede
+st.markdown("""
+📂 **Caminho para acesso:**  
+`Y:\\DEPTO CONTÁBIL\\BR02\\CONCILIAÇÕES\\Conciliações Renata`
+""")
