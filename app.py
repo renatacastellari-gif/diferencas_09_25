d58bdd7b5763a94342e4fa247ee558d4b893d906
renatacastellari-gif <renatacastellari@gmail.com>
2025-11-04 13:27:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -92,10 +92,11 @@ Conciliações dos Impostos
 
     ---
 
-    > **Objetivo:** Garantir o alinhamento entre os saldos fiscais e contábeis, prevenindo divergências nos registros.<br>
+    > **Objetivo:** Garantir o alinhamento entre os saldos fiscais e contábeis, prevenindo divergências nos registros.
        Desenvolvemos essa página para proporcionar acesso rápido e facilidade na visualização das conciliações.
 
     """)
+
 
 
 
